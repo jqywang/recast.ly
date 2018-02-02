b9496e396ce25e3add765b10793747eafcce6dbc
jqywang <jqywang.95@gmail.com>
2018-02-01 18:49:23 -0800

    implement http fetch instead of jquery ajax

--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,18 +1,35 @@
+// var searchYouTube = (options, callback) => {
+//   $.ajax({
+//     type: 'GET',
+//     data: $.extend({
+//       key: options.key,
+//       q: options.query,
+//       part: 'snippet',
+//       type: 'video'
+//     }, {maxResults: options.max}),
+//     timeout: 5000,
+//     url: 'https://www.googleapis.com/youtube/v3/search',
+//     success: (data) => {
+//       callback(data.items);
+//     },
+//     error: () => console.log('get request from search failed')
+//   });
+// };
+// window.searchYouTube = searchYouTube;
+
 var searchYouTube = (options, callback) => {
-  $.ajax({
-    type: 'GET',
-    data: $.extend({
-      key: options.key,
-      q: options.query,
-      part: 'snippet',
-      type: 'video'
-    }, {maxResults: options.max}),
-    timeout: 5000,
-    url: 'https://www.googleapis.com/youtube/v3/search',
-    success: (data) => {
+  var url = 'https://www.googleapis.com/youtube/v3/search';
+  url += '?part=snippet';
+  url += '&maxResults=' + options.max;
+  url += '&q=' + options.query;
+  url += '&type=video&videoEmbeddable=true';
+  url += '&key=' + options.key;
+  
+
+  fetch(url).then((response) => { return response.json(); })
+    .then (function(data) {
       callback(data.items);
-    },
-    error: () => console.log('get request from search failed')
-  });
+    });
 };
+
 window.searchYouTube = searchYouTube;
